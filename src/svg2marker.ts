d89c4a1d6069c4dcf0e4285d190717c02eff171b
zqlu <zqlu.cn@gmail.com>
2019-06-20 20:11:43 +0800

    feat: change api to return marker register function

--- src/svg2marker.ts
+++ src/svg2marker.ts
@@ -5,52 +5,59 @@ import svgPathParser from 'svg-path-parser';
 const parser: (path: string) => ISVGPathCmd[] = memoize(svgPathParser);
 
 /**
- *  Function to parse svg path to path array need by Marker symbol
+ *  Return function to register a Marker Symbol for give SVG Path
  *
  * @param svgPath SVG Path string
  * @param viewBoxWidth SVG view box width, default to 1024
  * @param viewBoxHeight SVG view box height, default to 1024
  */
-export function svg2marker(
+export function path2marker(
   svgPath: string,
-  markerCfg: IMarkerCfg,
   viewBoxWidth: number = 1024,
   viewBoxHeight: number = 1014,
-): (string | number)[][] {
-  const { x, y, r } = markerCfg;
-  const paths: ISVGPathCmd[] = parser(svgPath);
+): (x: number, y: number, r: number) => (string | number)[][] {
+  return (x: number, y: number, r: number): (string | number)[][] => {
+    const paths: ISVGPathCmd[] = parser(svgPath);
 
-  return paths.map((path: ISVGPathCmd) => {
-    const arr: (string | number)[] = [];
-    arr.push(path.relative === true ? path.code.toLowerCase() : path.code.toUpperCase());
-    const pairs: IPointPair[] = [[path.x1, path.y1], [path.x2, path.y2], [path.x, path.y]];
-    pairs.forEach((pair: IPointPair) => {
-      const [px, py] = pair;
-      if (px !== undefined) {
-        arr.push(
-          path.relative === true
-            ? (px / viewBoxWidth) * 2 * r
-            : x - r + r * 2 * (px / viewBoxWidth),
-        );
-      }
-      if (py !== undefined) {
-        arr.push(
-          path.relative === true
-            ? (py / viewBoxHeight) * 2 * r
-            : y - r + r * 2 * (py / viewBoxHeight),
-        );
-      }
+    return paths.map((path: ISVGPathCmd) => {
+      const arr: (string | number)[] = [];
+      arr.push(path.relative === true ? path.code.toLowerCase() : path.code.toUpperCase());
+      const pairs: IPointPair[] = [[path.x1, path.y1], [path.x2, path.y2], [path.x, path.y]];
+      pairs.forEach((pair: IPointPair) => {
+        const [px, py] = pair;
+        if (px !== undefined) {
+          arr.push(
+            path.relative === true
+              ? (px / viewBoxWidth) * 2 * r
+              : x - r + r * 2 * (px / viewBoxWidth),
+          );
+        }
+        if (py !== undefined) {
+          arr.push(
+            path.relative === true
+              ? (py / viewBoxHeight) * 2 * r
+              : y - r + r * 2 * (py / viewBoxHeight),
+          );
+        }
+      });
+
+      return arr;
     });
-
-    return arr;
-  });
+  };
 }
 
-export function iconfont2marker(icon: string, markCfg: IMarkerCfg): (string | number)[][] {
+/**
+ * Return function to register a Marker symbol for give svg file
+ *
+ * @param icon SVG file content
+ */
+export function svg2marker(
+  icon: string,
+): (x: number, y: number, r: number) => (string | number)[][] {
   const pathMatch: RegExpMatchArray | null = /<path\s+d="(.*?)"/i.exec(icon);
   const viewBoxMatch: RegExpExecArray | null = /viewBox="\d+\s+\d+\s+(\d+)\s+(\d+)"/i.exec(icon);
   if (pathMatch === null || pathMatch.length < 2) {
-    return [];
+    return (): (string | number)[][] => [];
   }
   let width: number = 1024;
   let height: number = 1024;
@@ -63,7 +70,7 @@ export function iconfont2marker(icon: string, markCfg: IMarkerCfg): (string | nu
     }
   }
 
-  return svg2marker(pathMatch[1], markCfg, width, height);
+  return path2marker(pathMatch[1], width, height);
 }
 
 export default svg2marker;
